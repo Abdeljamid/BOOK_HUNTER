f8da8e91007bc58e7c93b17b805bac360e0cba00
Majid <Ajouaoujamid@gmail.com>
2024-06-04 20:58:45 +0200

    integration zone dynamique + decoupe partials

--- app/config/params.php
+++ app/config/params.php
@@ -1,4 +1,11 @@
 <?php
+
+// initier zone dynamique 
+
+    $title ='';
+    $content ='';
+
+// connexion a la DB
     define('DB_HOST', 'localhost:3306') ;
     
     define('DB_NAME', 'book_hunter_2024') ;
