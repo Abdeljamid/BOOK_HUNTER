4f0e3f8734b4ce0066164538db38365980db0af9
Majid <Ajouaoujamid@gmail.com>
2024-06-04 20:32:30 +0200

    Structure mvc initial

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+// charger l'initialisation
+
+require_once '../core/init.php';
+
+// charger le router
+
+require_once '../app/routers/index.php';
+
+// charger le templates 
+
+require_once '../app/views/templates/index.php';
